56e3f14e2c77afc5b80502556595e492faf9463d
Richard Möhn <richard.moehn@posteo.de>
2017-03-26 14:43:02 +0900

    Finish Graant for Redmine so far

--- graant.py
+++ graant.py
@@ -7,12 +7,13 @@ import ast
 import codecs
 import collections
 from datetime import datetime
+import sys
 
 import unicodecsv as csv
 
 import graant_redmine
 
-csv.register_dialect('hledger', delimiter=',', quoting=csv.QUOTE_ALL)
+csv.register_dialect('hledger', delimiter=b',', quoting=csv.QUOTE_ALL)
 
 
 def read_config(path):
@@ -24,20 +25,21 @@ def split_account(a):
     return a.split(":")
 
 
-LogEntry = collections.namedtuple('LogEntry', ['date', 'is_uploaded', 'account',
-                'hours', 'comment'])
+LogEntry = collections.namedtuple('LogEntry', ['date', 'is_uploaded',
+                'account', 'account_path', 'hours', 'comment'])
 
 
 def log_entry(row_dict):
     return LogEntry(date=datetime.strptime(row_dict['date'], "%Y/%m/%d").date(),
                     is_uploaded=(row_dict['status'] == '*'),
                     account=row_dict['account'],
+                    account_path=row_dict['account'].split(':'),
                     hours=row_dict['amount'],
-                    comment=row_dict['posting_comment'])
+                    comment=row_dict['posting-comment'])
 
 
 processor_for = {'redmine': graant_redmine.make_processor,
-                 'nop': lambda _: (lambda x: x),}
+                 'nop': lambda _: (lambda _: None),}
 
 
 def init_processors(config):
@@ -45,12 +47,25 @@ def init_processors(config):
             for top_account, proc_config in config.items()}
 
 
+def process(processors, the_log_entry):
+    processors[the_log_entry.account_path[0]](the_log_entry)
+
+
 def process_journal(config_path, journal_path):
     config      = read_config(config_path)
+    exclusions  = {e for c in config.values() for e in c.get('exclude', [])}
     processors  = init_processors(config)
 
     with open(journal_path) as journal_f:
         reader = csv.DictReader(journal_f, dialect='hledger')
         for row_dict in reader:
+            if row_dict['account'] in exclusions:
+                continue
+
             the_log_entry = log_entry(row_dict)
             process(processors, the_log_entry)
+
+
+if __name__ == '__main__':
+    (config_p, journal_p) = sys.argv[1:]
+    process_journal(config_p, journal_p)
--- graant_redmine.py
+++ graant_redmine.py
@@ -3,19 +3,18 @@ from redmine import Redmine
 def split_account(a):
     return a.split(":")
 
-def make_redmine_processor(config):
+def make_processor(config):
     redmine = Redmine(config['redmine_url'], key=config['api_key'])
 
     def process(log_entry):
         if log_entry.is_uploaded:
             return
 
-        (_, issue_id, activity_name) = split_account(log_entry.account)
-        activity_id = config['act_name2act_id'][activity_name]
-        redmine.time_entry.create({'issue_id':      issue_id,
-                                   'activity_id':   activity_id,
-                                   'spent_on':      log_entry.date,
-                                   'hours':         float(log_entry.hours),
-                                   'comments':      log_entry.comment})
+        activity_id = config['act_name2act_id'][log_entry.account_path[2]]
+        redmine.time_entry.create(**{'issue_id':      log_entry.account_path[1],
+                                     'activity_id':   activity_id,
+                                     'spent_on':      log_entry.date,
+                                     'hours':         float(log_entry.hours),
+                                     'comments':      log_entry.comment})
 
     return process
